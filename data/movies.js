e99e3b9d44483c47db9411353f6384b8a1fa6767
Alexander Kulpin <alexandtoddy@gmail.com>
2022-04-21 00:27:52 -0400

    Movie Data Loading into Movie Page
    Movie data is now able to be fetched and loads up on the Movie page, currently only sample data

--- data/movies.js
+++ data/movies.js
@@ -1,7 +1,8 @@
 const mongoCollections = require('./../config/mongoCollections');
 const movies = mongoCollections.movies;
 
-async function addMovie(movieTitle, releaseYear, genre, rating, description, actors, directors, whereToWatch) {
+async function addMovie(movieTitle, releaseYear, genre, rating, description, actors, directors, whereToWatch)
+{
   console.log("inside addMovie");
   const moviesCollection = await movies();
   let dupMovie = await moviesCollection.findOne({movieTitle: movieTitle});
@@ -21,11 +22,31 @@ async function addMovie(movieTitle, releaseYear, genre, rating, description, act
   return {movieInserted: true};
 }
 
-async function test() {
+async function getMovie(id) 
+{	
+	/*
+	if (!id) throw "You must provide an id to search for";
+	if (typeof id !== 'string') throw "Id must be a string";
+	if (id.trim().length === 0) throw "Id cannot be an empty string or just spaces";
+  id = id.trim();
+  if (!ObjectId.isValid(id)) throw "Invalid object ID";
+	const moviesCollection = await movies();
+	const movie = await moviesCollection.findOne({ _id: ObjectId(id) });
+	*/
+	const moviesCollection = await movies();
+	let movie = await moviesCollection.findOne({'movieTitle': "MovieOne"});
+	if (movie === null) throw "No movie with that id";
+	return movie;
+}
+
+async function test() 
+{
   return {test: true};
 }
 
-module.exports = {
+module.exports = 
+{
   addMovie,
+	getMovie,
   test
 }
--- routes/movie.js
+++ routes/movie.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const router = express.Router();
+const data = require('../data');
+const moviesData = data.movies;
+const mongoose = require('mongoose');
 
-
-router.get('/', async (req, res) => {
-    res.render('posts/movie', { title: "Movie Page", allData: req.session.user })
+router.get('/', async (req, res) => 
+{	
+	try 
+	{
+		var movieId = '626025afc71d571026e1ca41';
+		//var movieId = mongoose.Types.ObjectId('626025afc71d571026e1ca41');
+		let searchMovie = await moviesData.getMovie(movieId);
+		res.render('posts/movie', { title: "Movie Page", allData: searchMovie });
+	} 
+	catch (e) 
+	{
+		res.status(500);
+		res.render('posts/private', { title: "Film Foray", error: e })
+		return;
+	}	
 });
 
 module.exports = router;
