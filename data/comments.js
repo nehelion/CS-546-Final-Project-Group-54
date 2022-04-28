e0512359efa615b42107c4dbe3a62b31a0a4a89e
Alexander Kulpin <alexandtoddy@gmail.com>
2022-04-28 19:07:29 -0400

    Design Movie Changes

--- data/comments.js
+++ data/comments.js
@@ -7,28 +7,30 @@ async function addComment(movie, username, comment)
 {
 	const moviesCollection = await movies();
 	
-	let newComment = {
+	let newComment = 
+	{
 		movieId: movie._id,
-    username: username,
-    comment: comment
-  }
+		username: username,
+		comment: comment
+	}
 	
 	movie.comments.push(newComment);
 	
 	const updatedInfo = await moviesCollection.updateOne(
-    { _id: movie._id },
-    { $set: movie }
-  );
-  if (updatedInfo.modifiedCount === 0) 
+		{ _id: movie._id },
+		{ $set: movie }
+	);
+	if (updatedInfo.modifiedCount === 0) 
 	{
-    throw 'could not update movies successfully';
-  }
+		throw 'could not update movies successfully';
+	}
 	
 	const commentsCollection = await comments();
 	
 	const insertDetails = await commentsCollection.insertOne(newComment);
-  if (insertDetails.insertedCount === 0) throw "Could not add movie, try again!"
-  return {commentInserted: true};
+	if (insertDetails.insertedCount === 0) throw "Could not add movie, try again!"
+	
+	return {commentInserted: true};
 }
 
 async function getAllComments(movie)
@@ -40,7 +42,7 @@ async function getAllComments(movie)
 		if(i % 5 == 0 && i == 0)
 		{
 			movieList.push({comment: 
-				"<div id='Page1'>"
+				"<div id='Page1' class='comment_pages_div'>"
 			});
 		}
 		else if(i % 5 == 0 && i > 0)
@@ -49,19 +51,19 @@ async function getAllComments(movie)
 			{
 				movieList.push({comment: 
 				
-					"<a href='#'" + 
+					"<a href='#' class='comment_pages_l'" + 
 					"onclick='return show("+'"Page'+((i/5)-1)+'"'+","+'"Page'+(i/5)+'"'+");" +
-					"'>Show page " + ((i/5) - 1) + "</a>" +
+					"'>< Page " + ((i/5) - 1) + "</a>" +
 					"<a href='#'"
 				});
 			}
 			movieList.push({comment: 
-				"<a href='#'" + 
+				"<a href='#' class='comment_pages_r'" + 
 				"onclick='return show("+'"Page'+((i/5)+1)+'"'+","+'"Page'+(i/5)+'"'+");" +
-				"'>Show page " + ((i/5) + 1) + "</a></div>"
+				"'>Page " + ((i/5) + 1) + " ></a></div>"
 			});
 			movieList.push({comment: 
-				"<div id='Page" + ((i/5)+1) + "' style='display:none'>"
+				"<div id='Page" + ((i/5)+1) + "' style='display:none' class='comment_pages_div'>"
 			});
 		}
 		movieList.push({comment: 
@@ -71,12 +73,12 @@ async function getAllComments(movie)
 			movie.comments[i].comment + 
 			"</div></div><br>"
 		});
-		if(i == movie.comments.length - 1)
+		if(i == movie.comments.length - 1 && i > 5)
 		{
 			movieList.push({comment: 
-				"<a href='#'" + 
+				"<a href='#' class='comment_pages_l'" + 
 				"onclick='return show("+'"Page'+Math.trunc(i/5)+'"'+","+'"Page'+Math.trunc((i/5)+1)+'"'+");" +
-				"'>Show page " + Math.trunc(i/5) + "</a></div>"
+				"'>< Page " + Math.trunc(i/5) + "</a></div>"
 			});
 		}
 	}
